9ecb7b0688efae480f2fdcdc2b6f1fc9951e92b8
Clayton Easterbrooks & Ryan Heupler <student@epicodus.com>
2015-08-11 15:25:54 -0700

    added picture upload

--- app.rb
+++ app.rb
@@ -3,14 +3,34 @@ require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/places')
 
+
 get('/') do
   @places_been = Place.all()
+  # attachment "info.txt"
+  # "store it"
   erb(:index)
 end
 
+post ('/upload') do
+"upload page"
+  end
+
+
+
+
 post('/places') do
-  place_name = params.fetch('place_name')
-  place = Place.new(place_name)
-  place.save()
-  erb(:places)
+
+
+
+    place_name = params.fetch('place_name')
+    place = Place.new(place_name)
+    place.save()
+
+
+
+    File.open('public/slideshow_pictures/' + params['file'][:filename], "w") do |f|
+      f.write(params['file'][:tempfile].read)
+      @pic_name = params['file'][:filename]
+    erb(:places)
+  end
 end
